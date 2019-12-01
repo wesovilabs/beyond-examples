db134986e81774ec25714c009d897be9fc01e4a8
Iván Corrales Solera <developer@wesovilabs.com>
2019-12-01 07:27:35 +0100

    feat: updating deps

---
+++ returning/vendor/github.com/wesovilabs/beyond/api/context/arg.go
@@ -0,0 +1,52 @@
+package context
+
+import (
+	"reflect"
+)
+
+// Arg contains attributes to efine an argument
+type Arg struct {
+	name  string
+	kind  string
+	value interface{}
+}
+
+// NewArg constructs an instance of arg
+func NewArg(name string, value interface{}) *Arg {
+	kind := ""
+
+	if value != nil {
+		kind = reflect.TypeOf(value).String()
+	}
+
+	return &Arg{
+		name:  name,
+		value: value,
+		kind:  kind,
+	}
+}
+
+// Name returns the argument name
+func (a *Arg) Name() string {
+	return a.name
+}
+
+// Value returns the argument value
+func (a *Arg) Value() interface{} {
+	return a.value
+}
+
+// Kind returns the argument type
+func (a *Arg) Kind() string {
+	return a.kind
+}
+
+// Is check if argument has the provided type
+func (a *Arg) Is(t reflect.Type) bool {
+	return a.kind == t.String()
+}
+
+// IsError check if argument is an error
+func (a *Arg) IsError() bool {
+	return a.kind == "error" || a.kind == "*errors.fundamental"
+}
--- settings/go.mod
+++ settings/go.mod
@@ -1,10 +1,8 @@
 module github.com/wesovilabs/beyond-examples/settings
 
 require (
-	github.com/wesovilabs/beyond master
+	github.com/wesovilabs/beyond v0.0.2-0.20191130075146-9d94e6b3e4c1
 	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
 )
-
-
 
 go 1.13
